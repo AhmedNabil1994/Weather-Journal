4b847584a63ae1f24b4c1e51ddd59535bab61467
Ahmed Nabil <94ahmed.nabil@gmail.com>
2022-10-05 18:35:56 +0200

    setup local server

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
+// Require Express to run server and routes
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+// Start up an instance of app
+const app = express();
+const port = 3000;
+
+/* Middleware*/
+// Cors for cross origin allowance
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// Initialize the main project folder
+app.use(express.static("website"))
+
+// Setup Server
+const server = app.listen(port , ()=>{
+    console.log(`server is running at:localhost:${port}`)
+})
+app.get("/hello" , (req,res)=>{
+    res.send("Hello World")
+})
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
-
-// Start up an instance of app
-
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-
-// Initialize the main project folder
-app.use(express.static('website'));
 
 
-// Setup Server
+
